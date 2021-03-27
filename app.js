ca5d6de280171daaa728309b5d883ac7470ab0e2
lausellv <lausellv@yahoo.com>
2021-03-27 13:20:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025', 'blue', 'yellow'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');  // to grab the span
+
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
